4331b4ed82e582df249f0d05627d8567f603cd5f
Chiraphat Techasiri <ctechasiri@gmail.com>
2026-06-08 21:22:27 -0700

    Initial Commit

---
+++ AIChatApp/AIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppApp.swift
+//  AIChatApp
+//
+//  Created by Chiraphat Techasiri on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
